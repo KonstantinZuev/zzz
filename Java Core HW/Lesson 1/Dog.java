f1d2443f1fef64ddb5d0100759c5abf8dbb83c49
KonstantinZuev <konstantin_1945@bk.ru>
2022-03-26 16:25:20 +0300

    JavaCoreLesson1HW1

---
+++ Java Core HW/Lesson 1/Dog.java
@@ -0,0 +1,12 @@
+/**
+*Java 2. Home work #1
+*
+*@author Zuev Konstantin
+*@version 26.03.2022
+*/
+
+public class Dog extends Animal {
+    public Dog(String name) {
+        super("Dog", name, 300, 40, 4);
+    }
+}
