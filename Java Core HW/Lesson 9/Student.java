fbe95ea4eaba21bcdf675a679b58eabd97a5a2e6
KonstantinZuev <konstantin_1945@bk.ru>
2022-04-26 20:25:07 +0300

    JavaCoreLesson9HW9

---
+++ Java Core HW/Lesson 9/Student.java
@@ -0,0 +1,42 @@
+/**
+*Java 2. Home work #9
+*
+*@author Zuev Konstantin
+*@version 26.04.2022
+*/
+
+mport java.util.List;
+
+public class Student {
+    private String name;
+    private List<Course> courses;
+
+    public Student(String name, List<Course> courses) {
+        this.name = name;
+        this.courses = courses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", courses=" + courses +
+                '}';
+    }
+}
